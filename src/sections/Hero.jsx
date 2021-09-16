141ed728455a1668ac822eed4b8a9dd99a0b10db
ZeldOcarina <mattia.r86@gmail.com>
2021-09-16 19:39:03 +0200

    Change hero video

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
       <div className="container">
         <h1>The #1 Mentor in the World for Dental Practices</h1>
         <p>Our program adds $500,000 to $1,000,000 for each office you manage in 12 months or less.</p>
-        <Video image={image} alt="blue rectangle" video="589909707" />
+        <Video image={image} alt="blue rectangle" video="606718682" />
       </div>
     </Wrapper>
   );
